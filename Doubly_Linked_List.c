7d6c7f0ca26e1fe3495858bc3cd539b31e12b2e7
Ganesh Pendyala <ganeshpendyala0314@gmail.com>
2022-11-22 09:42:16 +0530

    Changed

--- Doubly_Linked_List.c
+++ Doubly_Linked_List.c
@@ -6,6 +6,38 @@ int data;
 struct Node* next;
 struct Node* prev;
 };
+//Deletion in the middle of a Double Linked List
+struct Node* DeleteInbw(struct Node* head ,int index){
+    struct Node* p=head;
+    struct Node* q=head->next;
+    int i=1;
+    while(i!=index-1){
+        p=p->next;
+        q=q->next;
+        i++;
+    }
+    p->next=q->next;
+    q->next->prev=p;
+    free(q);
+    return head;
+}
+
+// Deletion at the End of a Double Linked List
+struct Node* DeleteAtLast(struct Node* tail){
+    struct Node* p=tail;
+    tail=tail->prev;
+    tail->next=NULL;
+    free(p);
+    return tail;
+}
+// Deletion at the start of a Double Linked List
+struct Node* DeleteAtFirst(struct Node* head){
+    struct Node* p=head;
+    head=head->next;
+    head->prev=NULL;
+    free(p);
+    return head;
+}
 // Insertion in middle of a Doubly Linked List.
 struct Node*InsertInbw(struct Node* head, int data,int index){
     struct Node* newnode=(struct Node*)malloc(sizeof(struct Node*));
@@ -78,9 +110,14 @@ int main(){
         tail=temp;
     }
     Display(head);
-    // DisplayBack(tail);
-    // head=InsertAtHead(head,12);
-    // tail=InsertAtTail(tail,12);
+    /*
+    DisplayBack(tail);
+    head=InsertAtHead(head,12);
+    tail=InsertAtTail(tail,12);
     head=InsertInbw(head,12,3);
+    head=DeleteAtFirst(head);
+    tail=DeleteAtLast(tail);
+    head=DeleteInbw(head,3);
+    */
     Display(head);
 }
